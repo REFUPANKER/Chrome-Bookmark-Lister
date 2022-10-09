33df75b7ac94b80431c7ea58600b70b9f9a10baf
REFUPANKER <68808212+REFUPANKER@users.noreply.github.com>
2022-10-09 21:47:26 +0300

    First Upload of project
    app only listing links 
    i can add folders to listing system

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Threading;
+static void cwl(Object msg)
+{
+    System.Console.WriteLine(msg);
+}
+static void cw(Object msg)
+{
+    System.Console.Write(msg);
+}
+
+cwl("App started");
+cwl("> Chrome Bookmark Finder/Lister");
+string bookmarkPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\";
+if (Directory.Exists(bookmarkPath + "Google\\Chrome\\") == false)
+{
+    cwl("Chrome Not exists in local appdata");
+    return;
+}
+else
+{
+    bookmarkPath += "Google\\Chrome\\User Data\\Default\\";
+    cwl("Bookmark Location Might be there >");
+    cw(bookmarkPath);
+    bookmarkPath += "Bookmarks";
+    cwl("");
+}
+try
+{
+    cwl(bookmarkPath);
+    // Google/Chrome/UserData/Default/Bookmark file
+    var reading = Task.Run(async () =>
+    {
+        cwl("Async read started\n");
+        return await File.ReadAllTextAsync(bookmarkPath);
+    });
+    String[] objs=reading.Result.Split(",");
+    for(int i=0;i<objs.Length;i++)
+    {
+        if (objs[i].Contains("url\":"))
+        {
+            objs[i]=objs[i].Replace(" ","").Replace("\"url\":","").Replace("}","").Replace("]","").Replace("\"","").Replace("\n","");
+            cwl(objs[i]);
+        }
+    }
+
+}
+catch (System.Exception excp)
+{
+    cwl("\nError\n");
+    cwl(excp);
+}
+finally
+{
+    cwl("\nApplication ended");
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Chrome Bookmark Lister
+1. gets chrome's bookmark file 
+2. reads as async
+3. lists links (in next updates i can maybe list links with folders)
+
+# Project details
+1. Made with C# - Dotnet 6
+2. By Refupanker
